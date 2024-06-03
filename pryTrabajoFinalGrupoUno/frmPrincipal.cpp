dbfba4be5c682b777371da73f67fab6248869dc0
albeto9 <albpg@DESKTOP-FAOHBOD>
2024-06-03 02:39:11 -0500

    Add project files.

---
+++ pryTrabajoFinalGrupoUno/frmPrincipal.cpp
@@ -0,0 +1,10 @@
+#include "frmPrincipal.h"
+#include "Windows.h"
+using namespace pryTrabajoFinalGrupoUno;
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew frmPrincipal());
+	return 0;
+}
